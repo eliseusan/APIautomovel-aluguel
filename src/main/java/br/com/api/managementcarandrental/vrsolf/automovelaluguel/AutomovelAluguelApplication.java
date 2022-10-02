5418812e0d4663e934c738e3b3773e93615fa14d
Eliseu <eliseusantico@gmail.com>
2022-10-01 22:18:07 -0300

    init commit

---
+++ src/main/java/br/com/api/managementcarandrental/vrsolf/automovelaluguel/AutomovelAluguelApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.managementcarandrental.vrsolf.automovelaluguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomovelAluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomovelAluguelApplication.class, args);
+	}
+
+}
